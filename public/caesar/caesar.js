aa554ed3e9e48d432d5c6de1e1a134986a8d023f
Ben Myers <ben.myers@okstate.edu>
2017-09-14 20:33:21 -0500

    Caesar typewriter

--- public/caesar/caesar.js
+++ public/caesar/caesar.js
@@ -4,6 +4,8 @@ var alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
 var offset = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
     'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
+var interval = 300
+
 function keyUp() {
     document.getElementById('key').value = "" + (parseInt(document.getElementById('key').value) + 1);
     populateAlphaOffset();
@@ -19,7 +21,7 @@ function populateAlphaStandard() {
         var ch = alpha[i];
         var border = "cell-has-right-border"
         if(i == 25) {border = "";}
-        alphaStandard.innerHTML += '<td id="off-' + ch + '" class="cell cell-has-bottom-border ' + border + '">' + ch + "</td>";
+        alphaStandard.innerHTML += '<td id="alpha-' + ch + '" class="cell cell-has-bottom-border ' + border + '">' + ch + "</td>";
     }
 }
 
@@ -53,7 +55,13 @@ function populateAlphaOffset() {
 function encrypt() {
     var newString = "";
     for(var i = 0; i < document.getElementById('input').value.length; i++) {
-        newString += offset[alpha.indexOf(document.getElementById('input').value.toUpperCase().charAt(i))];
+        var char = document.getElementById('input').value.toUpperCase().charAt(i)
+        if(alpha.indexOf(char) >= 0) {
+            newString += offset[alpha.indexOf(char)];
+        }
+        else {
+            newString += char
+        }
     }
     return newString;
 }
@@ -61,7 +69,13 @@ function encrypt() {
 function decrypt() {
     var newString = "";
     for(var i = 0; i < document.getElementById('output').value.length; i++) {
-        newString += alpha[offset.indexOf(document.getElementById('output').value.toUpperCase().charAt(i))];
+        var char = document.getElementById('output').value.toUpperCase().charAt(i)
+        if(offset.indexOf(char) >= 0) {
+            newString += alpha[offset.indexOf(char)];
+        }
+        else {
+            newString += char
+        }
     }
     return newString;
 }
@@ -71,9 +85,47 @@ function demoEncrypt() {
     var cipher = encrypt();
     console.log(cipher)
     for(var i = 0; i < cipher.length; i++) {
-        document.getElementById('output').value += cipher.charAt(i);
-
+        var call = "encryptTicker( '" + cipher.charAt(i) + "', '" + document.getElementById('input').value.toUpperCase().charAt(i) + "');"
+        console.log(call)
+        setTimeout(call, interval*i)
     }
+}
+
+function demoDecrypt() {
+    document.getElementById('input').value = "";
+    var plaintext = decrypt();
+    console.log(plaintext)
+    for(var i = 0; i < plaintext.length; i++) {
+        var call = "decryptTicker( '" + document.getElementById('output').value.toUpperCase().charAt(i) + "', '" + plaintext.charAt(i) + "');"
+        console.log(call)
+        setTimeout(call, interval*i)
+    }
+}
+
+function encryptTicker(currentCipherChar, currentPlaintextChar) {
+    var output = document.getElementById('output')
+    if(offset.indexOf(currentPlaintextChar) >= 0) {
+        document.getElementById('off-' + currentCipherChar).classList.add('demo-encrypt-current')
+        document.getElementById('alpha-' + currentPlaintextChar).classList.add('demo-encrypt-current')
+    }
+
+    output.value += currentCipherChar
+
+    setTimeout("document.getElementById('off-" + currentCipherChar + "').classList.remove('demo-encrypt-current')", interval)
+    setTimeout("document.getElementById('alpha-" + currentPlaintextChar + "').classList.remove('demo-encrypt-current')", interval)
+}
+
+function decryptTicker(currentCipherChar, currentPlaintextChar) {
+    var input = document.getElementById('input')
+    if(alpha.indexOf(currentPlaintextChar) >= 0) {
+        document.getElementById('off-' + currentCipherChar).classList.add('demo-decrypt-current')
+        document.getElementById('alpha-' + currentPlaintextChar).classList.add('demo-decrypt-current')
+    }
+
+    input.value += currentPlaintextChar
+
+    setTimeout("document.getElementById('off-" + currentCipherChar + "').classList.remove('demo-decrypt-current')", interval)
+    setTimeout("document.getElementById('alpha-" + currentPlaintextChar + "').classList.remove('demo-decrypt-current')", interval)
 }
 
 window.onload = function() {
